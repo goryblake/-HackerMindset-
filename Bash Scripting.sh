588d3f5a773a2a7a7b2fefcb5d155952eadd87cb
Bureiko <167917949+goryblake@users.noreply.github.com>
2024-08-11 17:21:32 -0300

    Add files via upload

---
+++ Bash Scripting.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir Melissa
+mkdir Melissa/defesa_cibernetica
+
+mkdir Melissa/defesa_cibernetica/hacker_mindset
+mkdir Melissa/defesa_cibernetica/linux
+mkdir Melissa/defesa_cibernetica/redes
+
+touch Melissa/defesa_cibernetica/hacker_mindset/notas.txt
+touch Melissa/defesa_cibernetica/linux/notas.txt
+touch Melissa/defesa_cibernetica/redes/notas.txt
+
+echo "Sem notas cadastradas" > Melissa/defesa_cibernetica/hacker_mindset/notas.txt
+echo "Sem notas cadastradas" > Melissa/defesa_cibernetica/linux/notas.txt
+echo "Sem notas cadastradas" > Melissa/defesa_cibernetica/redes/notas.txt
