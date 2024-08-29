c25ae69ac112e24f8b63c2b5e1f81ab43767d218
Bureiko <167917949+goryblake@users.noreply.github.com>
2024-08-29 03:24:32 -0300

    Delete atividade1.py

--- atividade1.py
+++
@@ -1,8 +0,0 @@
-# Função para ler e exibir as 10 primeiras senhas do arquivo
-def exibir_senhas():
-    with open('SecLists/Passwords/darkweb2017-top100.txt', 'r') as arquivo:
-        senhas = arquivo.readlines()
-        for senha in senhas[:10]:
-            print(senha.strip())
-
-exibir_senhas()
